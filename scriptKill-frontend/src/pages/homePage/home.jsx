e346ea4f9a3cc86a5814f595db54ca5b1458df07
watchhill101 <1443256811@qq.com>
2025-07-21 19:47:50 +0800

    合并

--- scriptKill-frontend/src/pages/homePage/home.jsx
+++ scriptKill-frontend/src/pages/homePage/home.jsx
@@ -1,10 +1,20 @@
-import { NavBar } from "antd-mobile";
-import { MoreOutline, LocationOutline } from 'antd-mobile-icons'
+import { NavBar, Swiper } from "antd-mobile";
+import { MoreOutline, LocationOutline } from "antd-mobile-icons";
+import img1 from "../../assets/images/1.jpeg";
+import img2 from "../../assets/images/2.jpeg";
 
 const Home = () => {
+  const imgList = [img1, img2];
+  const items = imgList.map((item) => (
+    <Swiper.Item>
+      <div key={item} style={{ height: "100%", width: "100%" }}>
+        <img src={item} alt="" style={{ height: "100%", width: "100%" }} />
+      </div>
+    </Swiper.Item>
+  ));
   const left = (
-    <div style={{ display: 'flex', alignItems: 'center',fontSize: 24 }}>
-      <LocationOutline style={{ marginRight: '8px' }} />
+    <div style={{ display: "flex", alignItems: "center", fontSize: 24 }}>
+      <LocationOutline style={{ marginRight: "8px" }} />
       <span>Yar沉浸式剧本杀</span>
     </div>
   );
@@ -18,8 +28,26 @@ const Home = () => {
   return (
     <>
       <div>
-        <NavBar back={null} style={{height: 44}} left={left} right={right}></NavBar>
+        <NavBar
+          back={null}
+          style={{ height: 44 }}
+          left={left}
+          right={right}
+        ></NavBar>
       </div>
+      <div>
+        <Swiper
+          loop
+          autoplay
+          onIndexChange={(i) => {
+            console.log(i, "onIndexChange1");
+          }}
+        >
+          {items}
+        </Swiper>
+      </div>
+      <div>个人信息</div>
+      <div></div>
     </>
   );
 };
