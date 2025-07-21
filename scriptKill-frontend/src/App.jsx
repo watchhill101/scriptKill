e1f58aaa36cffdd26379ed8765ad51a43acf0c3a
watchhill101 <1443256811@qq.com>
2025-07-21 14:36:51 +0800

    app

--- scriptKill-frontend/src/App.jsx
+++ scriptKill-frontend/src/App.jsx
@@ -1,36 +1,11 @@
-<<<<<<< Updated upstream
-import { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import RankingPage from './pages/RankingPage';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-
-function App() {
-  const [count, setCount] = useState(0);
-=======
-import Home from "./pages/homePage/home.jsx";
->>>>>>> Stashed changes
+import Home from "./pages/homePage/home";
 
 const App = () => {
   return (
-<<<<<<< Updated upstream
-    <Router>
-      <Routes>
-        {/* 默认路由重定向到排行榜页面 */}
-        <Route path="/" element={<Navigate to="/ranking" replace />} />
-        {/* 排行榜页面路由 */}
-        <Route path="/ranking" element={<RankingPage />} />
-      </Routes>
-    </Router>
-  );
-}
-=======
     <>
       <Home />
     </>
   );
 };
->>>>>>> Stashed changes
 
 export default App;
