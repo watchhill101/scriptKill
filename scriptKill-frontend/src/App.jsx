8c36a0414f3f3a0d56f2dd470d395276b6036f36
junmo-1 <3273213060@qq.com>
2025-07-21 10:21:46 +0800

    1

--- scriptKill-frontend/src/App.jsx
+++ scriptKill-frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Home_S from './pages/home_S';
-import Login_S from './pages/login_S';
-import Register_S from './pages/registe_Sr';
+import Home_S from './pages/home_S/home_S';
+import Login_S from './pages/home_S/login_S';
+import Register_S from './pages/home_S/registe_S';
 
 export default function App() {
   return (
