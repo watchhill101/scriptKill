a75834af0ae2c780157232113e3355611f2dd5d1
junmo-1 <3273213060@qq.com>
2025-07-22 21:56:27 +0800

    2

--- scriptKill-frontend/src/App.jsx
+++ scriptKill-frontend/src/App.jsx
@@ -1,10 +1,23 @@
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/loginPage/login.jsx';
+import Register from './pages/loginPage/register.jsx';
+import Home from './pages/homePage/home.jsx'; // 假设首页组件路径
 
 function App() {
   return (
-    <div>
-      h1
-    </div>
+    <Router>
+      <Routes>
+        {/* 登录页面路由 */}
+        <Route path="/login" element={<Login />} />
+        {/* 注册页面路由 */}
+        <Route path="/register" element={<Register />} />
+        {/* 首页路由 */}
+        <Route path="/home" element={<Home />} />
+        {/* 默认重定向到登录页 */}
+        <Route path="*" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
